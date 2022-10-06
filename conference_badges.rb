580286c076d562ce2005b3d0c39cc1dcdb79fbcc
Saung Oo Myat <saung.myat@gmail.com>
2022-10-05 21:47:42 -0400

    commit tree ab47fdda3bd868bc3a5adf18b68d65e27adf4429

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,34 @@
 # Write your code here.
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    names.map do |list|
+        "Hello, my name is #{list}."
+    end
+end
+
+def assign_rooms(attendees)
+    attendees.map.with_index(1) do |attendee, room_num|
+      "Hello, #{attendee}! You'll be assigned to room #{room_num}!"
+    end
+  end
+  
+  def printer(attendees)
+    batch_badge_creator(attendees).each do |badge|
+      puts badge
+    end
+  
+    assign_rooms(attendees).each do |assignment|
+      puts assignment
+    end
+  end
+
+
+
+badge_maker("Arel")
+batch_badge_creator(["Arel", "Carol"])
+assign_rooms(["Arel", "Carol"])
+printer(["Arel", "Carol"])
